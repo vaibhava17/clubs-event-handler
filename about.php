dee5fa7ecfbe05dd833d982cd89030b9fe1ace66
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-26 16:40:23 +0530

    name change

--- about.php
+++ about.php
@@ -16,7 +16,7 @@ include('plugins/head.php');
     <div class="about">
       <div class="inner-section">
         <h1>About Us</h1>
-        <h5 class="" style="color: white;">ABHIRUCHI (The Hobby Club) provides an appropriate platform to strive the excellence and revealed the true personality of leadership.  
+        <h5 class="" style="color: white;">Shri Siddhi Vinayak.  
 Co-curricular activities help students to develop problem-solving, reasoning, critical thinking, creative thinking, communication and collaborative abilities. The importance of co-curricular activities is immense. Therefore, we bring a platform where students can explore from pin to plane of our college co-curricular activities. On this platform, students can get to know about all the information regarding various Abhiruchi clubs, Abhiruchi interviews, screenings, events(like Invertia and Mascot) and other competitions. Participation helps students in emotional development, social skill development and overall personality development. This is why our platform also allows students to enroll themselves in any of these activities as per their interest using this particular website.
         </h5>
         <br>
