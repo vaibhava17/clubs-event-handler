bccdf09891fd865f66286b1acd0e75dfd91eaa87
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-12-04 06:36:11 +0530

    kalanjali

--- admin/kalanjalihandler.php
+++ admin/kalanjalihandler.php
@@ -5,19 +5,19 @@ $date=$_POST['date'];
 $context=$_POST['context'];
 
 
-$target="../data/kalanjali/";
+$target="data/kalanjali/";
 $file_path=$target.basename($_FILES['file']['name']);
 $file_name=$_FILES['file']['name'];
 $file_tmp=$_FILES['file']['tmp_name'];
-$file_store="../data/kalanjali/".$file_name;
+$file_store="data/kalanjali/".$file_name;
 
 move_uploaded_file($file_tmp, $file_store);
 
-$target="../data/kalanjali/";
+$target="data/kalanjali/";
 $image_path=$target.basename($_FILES['image']['name']);
 $image_name=$_FILES['image']['name'];
 $image_tmp=$_FILES['image']['tmp_name'];
-$image_store="../data/kalanjali/".$image_name;
+$image_store="data/kalanjali/".$image_name;
 
 move_uploaded_file($image_tmp, $image_store);
 
