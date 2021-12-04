51bf7f2dd984d87866620f65286347973018f540
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-12-04 15:29:07 +0530

    db

--- dbs/connect.php
+++ dbs/connect.php
@@ -1,17 +1,17 @@
 <?php
 //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-$host="localhost";
-$user="root";
-$password="";
-$dbname="ssvgi_ceh";
-$connect=mysqli_connect($host,$user,$password,$dbname);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $host="localhost";
+// $user="root";
+// $password="";
+// $dbname="ssvgi_ceh";
+// $connect=mysqli_connect($host,$user,$password,$dbname);
 ?>
